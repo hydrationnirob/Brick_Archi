f98d040d4e543f5b64972e2dacc552e345a246bf
Md Rakibull Haque <52385810+hydrationnirob@users.noreply.github.com>
2024-05-27 00:48:17 +0600

    refact

---
+++ bricks/feature/__brick__/lib/features/{{name.snakeCase()}}/widgets/{{name.snakeCase()}}_baseWidgets.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+Widget build{{name.pascalCase()}}Widget() {
+  return const CupertinoPageScaffold(
+    child: Center(
+      child: Text('{{name.pascalCase()}} Widget'),
+    ),
+  );
+}
