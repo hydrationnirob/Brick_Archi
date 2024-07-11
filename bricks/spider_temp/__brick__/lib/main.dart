3864f05a167b0d1d5f6efbc98c5b075b57526ddb
Md Rakibull Haque <52385810+hydrationnirob@users.noreply.github.com>
2024-07-11 20:47:17 +0600

    update

--- bricks/spider_temp/__brick__/lib/main.dart
+++ bricks/spider_temp/__brick__/lib/main.dart
@@ -1,41 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'Application/App.dart';
 
-import 'Config.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: Config.appName,
-        theme: ThemeData(
-          inputDecorationTheme: InputDecorationTheme(
-            hintStyle:  TextStyle(color: Config.unSelectedTextColor),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Config.seconderyTextColor),
-              borderRadius: BorderRadius.circular(5),
-            ),
-            contentPadding: const EdgeInsets.symmetric(
-              horizontal: 12,
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Config.primaryColor),
-              borderRadius: BorderRadius.circular(10),
-            ),
-          ),
-          brightness: Brightness.light,
-          colorScheme: ColorScheme.fromSeed(seedColor: Config.primaryColor),
-          useMaterial3: true,
-        ),
-        home: const Scaffold(
-          body: Center(
-            child: Text('Hello World'),
-          ),
-        )
-    );
-  }
+void main()  {
+   WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
 }
-
 
 
