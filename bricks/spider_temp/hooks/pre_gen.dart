fc53d728e5abb8b9794a50cbd00660f4060e786b
Md Rakibull Haque <52385810+hydrationnirob@users.noreply.github.com>
2024-07-11 21:00:51 +0600

    Update pre_gen.dart

--- bricks/spider_temp/hooks/pre_gen.dart
+++ bricks/spider_temp/hooks/pre_gen.dart
@@ -1,11 +1,1 @@
-import 'dart:io';
-import 'package:mason/mason.dart';
 
-void run(HookContext context) {
-  // Read vars.
-  final name = context.vars['appName'];
-
-  // Use the `Logger` instance.
-  context.logger.info('Hello $name!');
-
-}
